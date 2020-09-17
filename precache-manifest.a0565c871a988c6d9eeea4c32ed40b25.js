3ca9a23345bb48a54659bf95b47e0afaeb290295
nazar.fernos <nazarum98@gmail.com>
2020-09-17 23:27:34 +0300

    Updates

---
+++ precache-manifest.a0565c871a988c6d9eeea4c32ed40b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0a9b9d4c39a92c21dacaf6692d8d33",
+    "url": "/spd-test-task/index.html"
+  },
+  {
+    "revision": "d2c4abc1862e3cc7977f",
+    "url": "/spd-test-task/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "aa51e20d124b5589d828",
+    "url": "/spd-test-task/static/js/2.ace4003a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spd-test-task/static/js/2.ace4003a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c4abc1862e3cc7977f",
+    "url": "/spd-test-task/static/js/main.00c4652e.chunk.js"
+  },
+  {
+    "revision": "58a9259c9d146ac04d7a",
+    "url": "/spd-test-task/static/js/runtime-main.d1d28bf2.js"
+  }
+]);
